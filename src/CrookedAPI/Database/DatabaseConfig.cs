ebe067cecdd7b95d391d25e1e83cefedab3f11e6
Jasmin <abdulmaxine8@gmail.com>
2026-04-26 12:55:08 +0800

    change font ng Clothing Shop

--- src/CrookedAPI/Database/DatabaseConfig.cs
+++ src/CrookedAPI/Database/DatabaseConfig.cs
@@ -3,7 +3,7 @@ namespace CrookedAPI
     public static class DatabaseConfig
     {
         public static readonly string DatabaseName = "crooked1";
-        public static readonly string RootConnectionString = "Server=localhost;Uid=user;Pwd=;";
-        public static readonly string ConnectionString = $"Server=localhost;Database={DatabaseName};Uid=user;Pwd=;";
+        public static readonly string RootConnectionString = "Server=localhost;Uid=root;Pwd=;";
+        public static readonly string ConnectionString = $"Server=localhost;Database={DatabaseName};Uid=root;Pwd=;";
     }
 }  // Pag nag error database after mo mag login, gawin mo palitan mo yung "user" ng "root"
